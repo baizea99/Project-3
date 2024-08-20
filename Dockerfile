d245ed74eff163bd74f7f8df200707abc3f15892
Jeff Destine <jeff.destine@21.co>
2024-08-20 19:17:51 -0400

    docker build

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,20 @@
-FROM python:3.9-slim
+FROM python:3.9
 
 WORKDIR /app
 
 COPY requirements.txt .
 
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    gcc \
+    libffi-dev \
+    libssl-dev \
+    python3-dev
+
 RUN pip install --upgrade pip
 
 RUN pip install spacy
+
 
 RUN python -m spacy download en_core_web_sm
 
--- app.py
+++ app.py
@@ -1,25 +1,8 @@
-'This Module is to run streamlit'
 import streamlit as st
-'This Package is to unpack model'
-import pickle
+import pandas as pd
 
-# load model
-model = pickle.load(open('project3-sentiment-analysis.pkl', 'rb'))
+# Load data from Jupyter Notebook
+df = pd.read_csv("Dataset.csv")
 
-# create title
-st.title('Sentiment Analysis Model')
-
-review = st.text_input('Enter your text:')
-
-submit = st.button('Predict')
-
-if submit:
-    prediction = model.predict([review])
-
-    # print(prediction)
-    # st.write(prediction)
-
-    if prediction[0] == 'positive':
-        st.success('Positive Review')
-    else:
-        st.warning('Negative Review')
+st.title("My Streamlit App")
+st.write(df)
