f51ee43e342a981ea12ba4cc6145857752a732fb
Jeff Destine <jeff.destine@21.co>
2024-08-26 09:17:10 -0400

    all

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import streamlit as st
+import pickle
 import pandas as pd
 
-# Load data from Jupyter Notebook
-df = pd.read_csv("Dataset.csv")
+# load model
+model = pickle.load(open('Project3-sentiment-analysis.pkl', 'rb'))
 
-st.title("My Streamlit App")
-st.write(df)
+# create title
+st.title('Sentiment Analysis Model')
+
+review = st.text_input('Enter your review:')
+
+submit = st.button('Predict')
+
+if submit:
+    prediction = model.predict([review])
+
+    # print(prediction)
+    # st.write(prediction)
+
+    if prediction[0] == 'positive':
+        st.success('Positive Review')
+    else:
+        st.warning('Negative Review')
